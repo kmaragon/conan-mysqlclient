b8d02bf88d73d89a678eea844232abf5d5864f38
Hans Klabbers <hans@klabbers.nl>
2016-06-24 15:34:25 +0200

    Fixed an issue with a wrong option

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="hklabbers", pure_c=False)
-    builder.add_common_builds()
+    builder = ConanMultiPackager()
+    builder.add_common_builds(pure_c=False)
     builder.run()
