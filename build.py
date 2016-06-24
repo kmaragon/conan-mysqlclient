7867869a448f201347ad8af91818943c9ee78a55
Hans Klabbers <hans@klabbers.nl>
2016-06-24 15:16:43 +0200

    Change to trigger a new build on travis-ci

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-#    builder = ConanMultiPackager(username="hklabbers", use_docker=True)
     builder = ConanMultiPackager(username="hklabbers", pure_c=False)
     builder.add_common_builds()
     builder.run()
